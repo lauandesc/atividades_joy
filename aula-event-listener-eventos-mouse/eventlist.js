1574932117a9539fbc88063e0ad304fbdaeb4457
lauandesc <lauandes1987@gmail.com>
2023-05-11 16:55:46 -0300

    adicionei itens dentro da funçao para atualizar todos os valores

--- aula-event-listener-eventos-mouse/eventlist.js
+++ aula-event-listener-eventos-mouse/eventlist.js
@@ -10,17 +10,28 @@ let subtotalInfo = {
   valor: 11.66,
 };
 
-
+function atualizarDom(){
 quantidadeSubtotal.innerText = subtotalInfo.quantidade + " itens";
 valorSubtotal.innerText = subtotalInfo.valor;
+}
+
+atualizarDom()
 
 function adicionarUm(){
-  quantidadeProduto.value++
+  quantidadeProduto.value++;
+  subtotalInfo.quantidade = subtotalInfo.quantidade + 1;
+  subtotalInfo.valor = subtotalInfo.valor + 11.66;
+  atualizarDom()
 
 }
 
+
 function diminuirUm(){
-  quantidadeProduto.value = Number(quantidadeProduto.value) - 1
+  quantidadeProduto.value = Number(quantidadeProduto.value) - 1;
+  subtotalInfo.quantidade = subtotalInfo.quantidade - 1;
+  subtotalInfo.valor = subtotalInfo.valor - 11.66;
+  atualizarDom()
+
 
 }
 
