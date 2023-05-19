6a97c183ddb716682d1bbd20d95b5c0870bfcb75
LAUANDES DOS SANTOS DA CONCEIÇÃO <lauandes@dinfo.br>
2023-05-19 11:59:58 -0300

    validação senha em andamento

--- validação-formulários-01/validacaofomr1.js
+++ validação-formulários-01/validacaofomr1.js
@@ -15,45 +15,56 @@ usernameInput.addEventListener('blur', ()=>{
     usernameLabel.classList.remove('required-popup');
 })
 
-
+// ---------- VALIDAÇÃO USEREMAIL ---------- //
 
 let useremailInput = document.getElementById("email");
 let useremailLabel = document.querySelector('label[for="email"]');
 let useremailHelper = document.getElementById("email-helper");
-// Mostrar popup de campo obrigatório
-//useremailInput.addEventListener("focus", ()=>{
-  //  useremailLabel.classList.add('required-popup');
-//})
-// Mostrar popup de campo obrigatório
-//useremailInput.addEventListener("blur", ()=>{
-  //  useremailLabel.classList.remove('required-popup');
-//})
 
-function mostrarPopup(input, label){
-    input.addEventListener("focus", ()=>{
-        label.classList.add('required-popup');
-    })
+useremailInput.addEventListener('focus', ()=>{
+  useremailLabel.classList.add('required-popup')
+
+useremailInput.addEventListener('blur',()=>{
+  useremailLabel.classList.remove('required-popup')
+} )
+})
+
+// ---------- VALIDAÇÃO SENHA ---------- //
+
+let usersenhaInput = document.getElementById('senha');
+let usersenhaLabel = document.querySelector('label[for="senha"]');
+let usersenhaHelper = document.getElementById('senha-helper');
+
+usersenhaInput = addEventListener('focus', () =>{
+  usersenhaLabel = classList.add('required-popup')
+})
+
+
+//function mostrarPopup(input, label){
+  //  input.addEventListener("focus", ()=>{
+    //    label.classList.add('required-popup');
+    //})
     // Mostrar popup de campo obrigatório
-    input.addEventListener("blur", ()=>{
-        label.classList.remove('required-popup');
-    })
-}
+   // input.addEventListener("blur", ()=>{
+     //   label.classList.remove('required-popup');
+    //})
+//}
 
 
 
 // Validar valor do input
-usernameInput.addEventListener('change', (event)=>{
-   let unameValue = event.target.value;
-   if(unameValue.length < 3){
-    usernameInput.classList.add('correct');
-    useremailInput.classList.add('error');
-    usernameHelper.innerText= "o nome deve ter pelo menos 3 caracteses"
-    useremailHelper.classList.add('visible');
-   }else {
-    usernameInput.classList.add('correct');
-    useremailHelper.classList.remove('visible');
-   }
+// usernameInput.addEventListener('change', (event)=>{
+//    let unameValue = event.target.value;
+//    if(unameValue.length < 3){
+//     usernameInput.classList.add('correct');
+//     useremailInput.classList.add('error');
+//     usernameHelper.innerText= "o nome deve ter pelo menos 3 caracteses"
+//     useremailHelper.classList.add('visible');
+//    }else {
+//     usernameInput.classList.add('correct');
+//     useremailHelper.classList.remove('visible');
+//    }
 
-});
+// });
 
 
